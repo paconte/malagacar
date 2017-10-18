abcf67ca1f284bbaabb312758730109afcd5d494
paconte <paconte@gmail.com>
2017-10-18 22:13:16 +0200

    Second version of prototype with a rent car page.

--- rentcar/views.py
+++ rentcar/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    from django.http import HttpResponse
-    return HttpResponse("Hello, world. You're at the rentcar index.")
+    if request.method == 'POST':
+        print("TODO BIEN")
+        return render(request, 'rent.html')
+    else:
+        print("TODO MAL")
+        return render(request, 'landing.html')
+
+
+def rent(request):
+    if request.method == 'POST':
+        print("TODO BIEN")
+        return render(request, 'rent.html')
+
