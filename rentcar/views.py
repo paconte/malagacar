ad80c797101f6243744d91fcf87e514a90793cd1
paconte <paconte@gmail.com>
2017-10-29 12:12:42 +0100

    Remove unused debug prints.

--- rentcar/views.py
+++ rentcar/views.py
@@ -3,16 +3,13 @@ from django.shortcuts import render
 
 def index(request):
     if request.method == 'POST':
-        print("TODO BIEN")
         return render(request, 'rent.html')
     else:
-        print("TODO MAL")
         return render(request, 'landing.html')
 
 
 def rent(request):
     if request.method == 'POST':
-        print("TODO BIEN")
         return render(request, 'rent.html')
 
 
