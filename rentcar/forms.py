06d3fb5f6a5b417100b31db42008c6e77b67a4af
paconte <paconte@gmail.com>
2017-11-05 13:22:30 +0100

    Form and validators python files.

---
+++ rentcar/forms.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .validators import DATE_FORMAT, arrival_date_validator, departure_date_validator, hours_validator, \
+    minutes_validator, get_min_arrival_date, get_max_arrival_date
+
+
+class SearchForm(forms.Form):
+
+    locations_list = [('Málaga Airport', 'Málaga Airport')]
+    hours_list = [('--', '--'), ('00', '00'), ('01', '01'), ('02', '02'), ('03', '03'), ('04', '04'), ('05', '05'),
+                  ('06', '06'), ('07', '07'), ('08', '08'), ('09', '09'), ('10', '10'), ('11', '11'), ('12', '12'),
+                  ('13', '13'), ('14', '14'), ('15', '15'), ('16', '16'), ('17', '17'), ('18', '18'), ('19', '19'),
+                  ('20', '20'), ('21', '21'), ('22', '22'), ('23', '23')]
+    minutes_list = [('--', '--'), ('00', '00'), ('15', '15'), ('30', '30'), ('45', '45')]
+
+    arrival_initial_date = get_min_arrival_date().strftime(DATE_FORMAT)
+    departure_initial_date = get_max_arrival_date().strftime(DATE_FORMAT)
+
+    arrival_location = forms.ChoiceField(choices=locations_list, label='Arrival')
+    arrival_date = forms.CharField(initial=arrival_initial_date, validators=[arrival_date_validator])
+    arrival_hours = forms.ChoiceField(choices=hours_list, validators=[hours_validator])
+    arrival_minutes = forms.ChoiceField(choices=minutes_list, validators=[minutes_validator])
+
+    departure_location = forms.ChoiceField(choices=locations_list)
+    departure_date = forms.CharField(initial=departure_initial_date, validators=[departure_date_validator])
+    departure_hours = forms.ChoiceField(choices=hours_list, validators=[hours_validator])
+    departure_minutes = forms.ChoiceField(choices=minutes_list, validators=[minutes_validator])
---
+++ rentcar/validators.py
@@ -0,0 +1,51 @@
+import datetime
+import pytz
+from django.core.exceptions import ValidationError
+
+DATE_FORMAT = "%d/%m/%Y"
+TIME_ZONE = pytz.timezone('Europe/Berlin')
+MIN_ARRIVAL_DATE_DELTA = 1
+MIN_DEPARTURE_DATE_DELTA = MIN_ARRIVAL_DATE_DELTA + 7
+MINUTES_LIST = ['00', '15', '30', '45']
+HOURS_LIST = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '14', '15', '16', '17',
+              '18', '19', '20', '21', '22', '23']
+
+
+def get_min_arrival_date(delta=MIN_ARRIVAL_DATE_DELTA):
+    arrival_delta = datetime.timedelta(days=delta)
+    now = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    arrival_initial_date = now + arrival_delta
+    return arrival_initial_date
+
+
+def get_max_arrival_date():
+    return get_min_arrival_date(MIN_DEPARTURE_DATE_DELTA)
+
+
+def arrival_date_validator(value):
+    min_arrival_date = get_min_arrival_date()
+    arrival_date = datetime.datetime.strptime(value, DATE_FORMAT)
+    if not arrival_date >= min_arrival_date:
+        msg = u"Wrong arrival date"
+        raise ValidationError(msg)
+
+
+def departure_date_validator(value):
+    min_departure_date = get_max_arrival_date()
+    departure_date = datetime.datetime.strptime(value, DATE_FORMAT)
+    if not departure_date >= min_departure_date:
+        msg = u"Wrong departure date"
+        raise ValidationError(msg)
+
+
+def minutes_validator(value):
+    if value not in MINUTES_LIST:
+        msg = u"Wrong minutes"
+        raise ValidationError(msg)
+
+
+def hours_validator(value):
+    print(type(value), value)
+    if value not in HOURS_LIST:
+        msg = u"Wrong minutes"
+        raise ValidationError(msg)
