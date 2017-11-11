876ef5a6bbfd2977a1e0d21b94cc1659b873b21c
paconte <paconte@gmail.com>
2017-11-11 16:05:26 +0100

    Improve models: only one car class and add prototype for booking.

--- rentcar/models.py
+++ rentcar/models.py
@@ -1,9 +1,11 @@
+import datetime
 from django.db import models
+from rentcar.validators import convert_date
 
 CAR_GROUPS_CHOICES = (('Group A', 'Group A'), ('Group B', 'Group B'))
 CAR_BRANDS_CHOICES = (('Renault', 'Renault'), ('Volkswagen', 'Volkswagen'))
 CAR_ENERGY_CHOICES = (('Diesel', 'Diesel'), ('Petrol', 'Petrol'))
-# CAR_MECHANIC_CHOICES = (('Automatic', 'Automatic'), ('Manual', 'Manual'))
+CAR_MECHANIC_CHOICES = (('Automatic', 'Automatic'), ('Manual', 'Manual'))
 
 
 class CarGroup(models.Model):
@@ -32,8 +34,9 @@ class Car(models.Model):
     model = models.CharField(max_length=32)
     places = models.PositiveSmallIntegerField()
     air_conditioning = models.BooleanField(default=True)
-    automatic = models.BooleanField(default=False)
+    mechanic = models.CharField(choices=CAR_MECHANIC_CHOICES, max_length=16)
     fuel = models.CharField(choices=CAR_ENERGY_CHOICES, max_length=16)
+    available = models.BooleanField(default=True)
 
     @property
     def name(self):
@@ -43,11 +46,39 @@ class Car(models.Model):
         return self.name
 
 
-class CarInventory(models.Model):
-    car = models.ForeignKey(Car)
-    quantity = models.PositiveSmallIntegerField(default=0)
-    available = models.PositiveSmallIntegerField(default=0)
-
-
 class Booking(models.Model):
     car = models.ForeignKey(Car)
+    session = models.ForeignKey('sessions.Session')
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    date_from = models.DateTimeField()
+    date_until = models.DateTimeField()
+    time_period = models.PositiveSmallIntegerField()
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+
+def search_available_cars(arrival_date, arrival_hours, arrival_minutes,
+                          departure_date, departure_hours, departure_minutes):
+
+    date_from = convert_date(arrival_date).replace(hour=int(arrival_hours), minute=int(arrival_minutes))
+    date_until = convert_date(departure_date).replace(hour=int(departure_hours), minute=int(departure_minutes))
+    cars = Car.objects.filter(available=True)
+    available_cars = list()
+
+    for car in cars:
+        # only cars available must be appear in the search
+        # only cars without a date_from in the desired period must appear in the search
+        # only cars without a date_until in the desired period must appear in the search
+        bookings = Booking.objects.filter(car=car)\
+            .filter(date_from__gte=date_from).filter(date_from__lte=date_until)\
+            .filter(date_until__gte=date_from).filter(date_until__lte=date_until)
+        if not bookings:
+            available_cars.append(car)
+
+    print(date_from, date_until)
+    print(cars, available_cars)
+    print(bookings)
+    return available_cars
+
