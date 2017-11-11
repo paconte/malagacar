ce44d16607b768f8e15c5da6c74ab85a4306f699
paconte <paconte@gmail.com>
2017-11-11 16:06:34 +0100

    Add logic when the search form is valid.

--- rentcar/views.py
+++ rentcar/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 
 from rentcar.forms import SearchForm
+from rentcar.models import search_available_cars
 
 
 def index(request):
     if request.method == 'POST':
         search_form = SearchForm(request.POST)
+        print(search_form.fields['arrival_date'])
         if search_form.is_valid():
-            return render(request, 'landing.html', {'form': search_form, 'search_result': list('a')})
+            available_cars = search_available_cars(
+                request.POST['arrival_date'],
+                request.POST['arrival_hours'],
+                request.POST['arrival_minutes'],
+                request.POST['departure_date'],
+                request.POST['departure_hours'],
+                request.POST['departure_minutes'])
+            return render(request, 'landing.html', {'form': search_form, 'available_cars': available_cars})
         else:
             return render(request, 'landing.html', {'form': search_form, 'search_result': list()})
     else:
