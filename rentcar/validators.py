dfa55a06e1349e3513384cb3f63d91ec84baa71b
paconte <paconte@gmail.com>
2017-11-11 16:03:34 +0100

    Add and use function for converting dates.

--- rentcar/validators.py
+++ rentcar/validators.py
@@ -7,8 +7,8 @@ TIME_ZONE = pytz.timezone('Europe/Berlin')
 MIN_ARRIVAL_DATE_DELTA = 1
 MIN_DEPARTURE_DATE_DELTA = MIN_ARRIVAL_DATE_DELTA + 7
 MINUTES_LIST = ['00', '15', '30', '45']
-HOURS_LIST = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '14', '15', '16', '17',
-              '18', '19', '20', '21', '22', '23']
+HOURS_LIST = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+              '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
 
 
 def get_min_arrival_date(delta=MIN_ARRIVAL_DATE_DELTA):
@@ -18,21 +18,21 @@ def get_min_arrival_date(delta=MIN_ARRIVAL_DATE_DELTA):
     return arrival_initial_date
 
 
-def get_max_arrival_date():
+def get_min_departure_date():
     return get_min_arrival_date(MIN_DEPARTURE_DATE_DELTA)
 
 
 def arrival_date_validator(value):
     min_arrival_date = get_min_arrival_date()
-    arrival_date = datetime.datetime.strptime(value, DATE_FORMAT)
+    arrival_date = convert_date(value)
     if not arrival_date >= min_arrival_date:
         msg = u"Wrong arrival date"
         raise ValidationError(msg)
 
 
 def departure_date_validator(value):
-    min_departure_date = get_max_arrival_date()
-    departure_date = datetime.datetime.strptime(value, DATE_FORMAT)
+    min_departure_date = get_min_departure_date()
+    departure_date = convert_date(value)
     if not departure_date >= min_departure_date:
         msg = u"Wrong departure date"
         raise ValidationError(msg)
@@ -48,3 +48,7 @@ def hours_validator(value):
     if value not in HOURS_LIST:
         msg = u"Wrong minutes"
         raise ValidationError(msg)
+
+
+def convert_date(value):
+    return datetime.datetime.strptime(value, DATE_FORMAT)
