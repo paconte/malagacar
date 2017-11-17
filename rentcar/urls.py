c13f267287246b412a5c06de25316bd8337adfa5
paconte <paconte@gmail.com>
2017-11-17 18:25:43 +0100

    Add url and view for booking confirmation.

--- rentcar/urls.py
+++ rentcar/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^booking/(?P<car_id>\d+)/$', views.booking, name='booking'),
+    url(r'^confirmation$', views.booking_confirmation, name='booking_confirmation'),
 ]
--- rentcar/views.py
+++ rentcar/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
-from rentcar.forms import SearchForm
-from rentcar.models import search_available_cars, get_car
+from rentcar.forms import SearchForm, BookingForm
+from rentcar.models import search_available_cars, get_car, Booking
 from rentcar.validators import calculate_total_rent_days, convert_dates, date_to_string, extract_time_from_date
 
 
@@ -18,6 +19,16 @@ def index(request):
             prices = list()
             for car in available_cars:
                 prices.append(car.calculate_price(rent_days))
+            # create booking form
+            booking_forms = list()
+            for car in available_cars:
+                date_from, date_until = convert_dates(a, b, c, d, e, f)
+                price = car.calculate_price(rent_days)
+                reservation = Booking(date_from=date_from, date_until=date_until, time_period=rent_days,
+                                      car=car, total=price)
+                booking_form = BookingForm(instance=reservation)
+                booking_forms.append(booking_form)
+
             # add prices to the available cars
             cars_with_prices = zip(available_cars, prices)
             # render
@@ -32,30 +43,53 @@ def booking(request, car_id):
     if request.method == 'POST':
         search_form = SearchForm(request.POST)
         if search_form.is_valid():
+            # retrieve the car data from db
+            car = _get_car(request, car_id, search_form)
+            # calculate the dates
             a, b, c, d, e, f = _get_dates_arguments(request)
-
-            car = get_car(car_id)
-
             date_from, date_until = convert_dates(a, b, c, d, e, f)
             str_date_from = date_to_string(date_from)
             str_date_until = date_to_string(date_until)
             str_time_from = extract_time_from_date(date_from)
             str_time_until = extract_time_from_date(date_until)
-
+            # calculate price and total rented days
             rent_days = calculate_total_rent_days(a, b, c, d, e, f)
             price = car.calculate_price(rent_days)
+            # pre fill out the reservation
+            reservation = Booking(date_from=date_from, date_until=date_until, time_period=rent_days,
+                                  car=car, total=price)
+            booking_form = BookingForm(instance=reservation)
 
             return render(request, 'booking.html',
-                          {'car': car, 'date_from': date_from, 'date_until': date_until,
+                          {'car': car, 'booking_form': booking_form,
                            'str_date_from': str_date_from, 'str_date_until': str_date_until,
-                           'str_time_from': str_time_from, 'str_time_until': str_time_until,
-                           'rent_days': rent_days, 'price': price})
+                           'str_time_from': str_time_from, 'str_time_until': str_time_until})
         else:
-            return render(request, 'landing.html', {'form': SearchForm()})
+            return render(request, 'landing.html', {'form': search_form})
     else:
         return render(request, 'landing.html', {'form': SearchForm()})
+
+
+def booking_confirmation(request):
+    if request.method == 'POST':
+        booking_form = BookingForm(request.POST)
+        if booking_form.is_valid():
+            return render(request, 'confirmation.html')
+        else:
+            return render(request, 'booking.html', {'booking_form': booking_form})
 
 
 def _get_dates_arguments(request):
     return (request.POST['arrival_date'], request.POST['arrival_hours'], request.POST['arrival_minutes'],
             request.POST['departure_date'], request.POST['departure_hours'], request.POST['departure_minutes'])
+
+
+def _get_car(request, car_id, search_form):
+    try:
+        car = get_car(car_id)
+    except ObjectDoesNotExist:
+        return render(request, 'landing.html', {'form': search_form})
+    except MultipleObjectsReturned:
+        return render(request, 'landing.html', {'form': search_form})
+    return car
+
